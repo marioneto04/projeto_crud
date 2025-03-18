56e76c81de1bd47652de4e19304c2b775e67edf6
jabesz <jabesvieira4@gmail.com>
2025-03-18 08:32:17 -0300

    Criação da classe resposta modelo

---
+++ backend/produtos/src/main/java/br/com/crud/produtos/modelo/RespostaModelo.java
@@ -0,0 +1,12 @@
+package br.com.crud.produtos.modelo;
+
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class RespostaModelo {
+    private String resposta;
+}
