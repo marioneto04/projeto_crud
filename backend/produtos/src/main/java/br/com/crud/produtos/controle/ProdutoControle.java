1406066a07591106ad4638bd284f593d81346cb7
jabes <jabesvieira4@gmail.com>
2025-03-31 16:36:34 -0300

    Rota para remover criada

--- backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
@@ -2,12 +2,15 @@ package br.com.crud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.crud.produtos.modelo.ProdutoModelo;
+import br.com.crud.produtos.modelo.RespostaModelo;
 import br.com.crud.produtos.servico.ProdutoServico;
 
 public class ProdutoControle {
@@ -15,24 +18,30 @@ public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
 
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+        return ps.remover(codigo);
+
+    }
+
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
         return ps.cadastrarAlterar(pm, "cadastrar");
     }
-    
+
     @PutMapping("/alterar")
-    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
         return ps.cadastrarAlterar(pm, "alterar");
     }
 
     @GetMapping("/listar")
-    public Iterable<ProdutoModelo> listar(){
+    public Iterable<ProdutoModelo> listar() {
         return ps.listar();
     }
 
     @GetMapping("/")
-    public String rota(){
+    public String rota() {
         return "A API está funcionando!";
     }
-    
+
 }
