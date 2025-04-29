48ba0d286dee17a2f6b466adfd7f52439561bed3
jabez <jabesvieira4@gmail.com>
2025-04-29 11:32:25 -0300

    .

--- frontend/produtos/src/Tabela.jsx
+++ frontend/produtos/src/Tabela.jsx
@@ -8,19 +8,20 @@ function Tabela({ vetor }) {
           <th>Marca</th>
           <th>Selecionar</th>
         </tr>
-        <tbody>
-          {vetor.map((obj, indice) => (
-            <tr key={indice}>
-              <td>{indice + 1}</td>
-              <td>{obj.nome}</td>
-              <td>{obj.marca}</td>
-              <td>
-                <button className="btn btn-success"></button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
       </thead>
+
+      <tbody>
+        {vetor.map((obj, indice) => (
+          <tr key={indice}>
+            <td>{indice + 1}</td>
+            <td>{obj.nome}</td>
+            <td>{obj.marca}</td>
+            <td>
+              <button className="btn btn-success"></button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
